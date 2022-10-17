2bb7a460f340be4650d4881004c3ac0d18346ee0
fatimap1986 <fatimapugliese@yahoo.com.ar>
2022-10-17 20:20:18 -0300

    primer css

---
+++ app2.js
@@ -0,0 +1,13 @@
+const express = require ("express")
+const app = express ()
+const path = require ("path")
+
+app.use (express.static ("public"))
+
+const port = process.env.PORT || 3030
+
+app.listen (port, () => console.log ("Servidor corriendo en el puerto ${port}"))
+
+app.get ("/", (req, res) => (res.sendFile (path.join (__dirname, "./index.html"))))
+
+app.get ("/register", (req, res) => (res.sendFile (path.join (__dirname, "./register.html"))))
