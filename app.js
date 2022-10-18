ee260bc6922db1bc4b9f24d9f92c7a4047a0a1bb
Tiago Miste <tiagomiste@gmail.com>
2022-10-17 22:34:14 -0300

    arreglando index

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ const path = require ("path")
 
 app.use (express.static ("public"))
 
-const port = process.env.PORT || 3030
+const port = process.env.PORT || 3030;
+app.listen(port, () => console.log("Servidor corriendo en el puerto ${http://localhost:3030}"));
 
-app.listen (port, () => console.log ("Servidor corriendo en el puerto ${port}"))
+app.get ("/", (req, res) => {
+    res.sendFile (path.resolve(__dirname, "./views/index.html"))
+})
 
-app.get ("/", (req, res) => (res.sendFile (path.join (__dirname, "./index.html"))))
+app.get ("/register", (req, res) => {
+    res.sendFile (path.join(__dirname, "./views/register.html"))
+})
 
-app.get ("/register", (req, res) => (res.sendFile (path.join (__dirname, "./register.html"))))
-
-app.get ("/login", (req, res) => (res.sendFile (path.join (__dirname, "./login.html"))))
+app.get ("/login", (req, res) => {
+    res.sendFile (path.join(__dirname, "./views/login.html"))
+})
