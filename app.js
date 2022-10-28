ae6fd04f0e7e2f3a6189de2b910677c96c4446e6
fatimap1986 <fatimapugliese@yahoo.com.ar>
2022-10-28 18:45:23 -0300

    finalizando segundo sprint

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.listen(3020)
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/register.html'))
-});
+const express = require ("express")
+const app = express ()
+const path = require ("path")
 
-app.use(express.static('public'))
+app.use (express.static ("public"))
+
+const port = process.env.PORT || 3030;
+app.listen(port, () => console.log("Servidor corriendo en el puerto ${http://localhost:3030}"));
+
+app.get ("/", (req, res) => {
+    res.sendFile (path.resolve(__dirname, "./views/index.html"))
+})
+
+app.get ("/register", (req, res) => {
+    res.sendFile (path.join(__dirname, "./views/register.html"))
+})
+
+app.get ("/login", (req, res) => {
+    res.sendFile (path.join(__dirname, "./views/login.html"))
+})
+
+app.get ("/cart", (req, res) => {
+    res.sendFile (path.join(__dirname, "./views/cart.html"))
