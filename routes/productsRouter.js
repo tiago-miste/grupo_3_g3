ca5710187a75dc00a0776f0644925b0588dce54a
fatimap1986 <fatimapugliese@yahoo.com.ar>
2022-12-05 12:23:44 -0300

    agregando rutas

---
+++ routes/productsRouter.js
@@ -0,0 +1,17 @@
+// ************ Require's ************
+const express = require('express');
+const router = express.Router();
+
+const productsController = require('../controllers/productsController')
+
+const uploadFile = require('../middlewares/multerMiddleware')
+
+router.get('/', productsController.index)
+router.get('/create', productsController.create)
+router.post('/', uploadFile.single('imgFile'), productsController.store)
+router.get('/detail/:id', productsController.detail)
+router.get('/detail/edit/:id', productsController.edit)
+router.put('/detail/edit/:id', uploadFile.single('imgFile'), productsController.update)
+router.delete('/detail/delete/:id', productsController.destroy)
+
+module.exports = router
