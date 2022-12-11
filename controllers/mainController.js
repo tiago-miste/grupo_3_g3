bbcdc0caccb9fb61451a1fff554fef8144ad1ec8
fatimap1986 <fatimapugliese@yahoo.com.ar>
2022-12-11 15:36:20 -0300

    correccion

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -2,8 +2,8 @@ const controller = {
     index: (req, res) => {res.render ('index')},
     register: (req, res) => {res.render('register')},
     login: (req, res) => {res.render('logIn')},
-    products: (req, res) => {res.render ('products')},
-    cart: (req, res) => {res.render('cart')},
-    create: (req, res) => {res.render('productsFormCreate')}}
-    
-    module.exports = controller;
+    cart: (req, res) => {res.render('cart')}
+}
+
+module.exports = controller
+
