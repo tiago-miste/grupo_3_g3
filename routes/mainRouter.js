9e7822699978fa558f49e70bcd1ad3c6fca7728a
fatimap1986 <fatimapugliese@yahoo.com.ar>
2022-12-11 15:42:07 -0300

    correccion

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -8,7 +8,5 @@ router.get('/', mainController.index);
 router.get('/register', mainController.register)
 router.get('/login', mainController.login)
 router.get('/cart', mainController.cart)
-router.get('/create', mainController.create)
-router.get('/products', mainController.products)
 
 module.exports = router;
