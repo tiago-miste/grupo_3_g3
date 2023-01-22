06ccc07ce804fba32d8a51d6da7a69e610f881ba
fatimap1986 <fatimapugliese@yahoo.com.ar>
2023-01-22 12:34:21 -0300

    correccion

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -12,14 +12,4 @@ router.post('/products/detail/update/:id', productsController.update);
 router.get('/products/detail/delete/:id', productsController.delete);
 router.post('/products/detail/delete/:id', productsController.destroy);
 
-
-router.get('/', productsController.products)
-router.get('/create', productsController.create)
-router.post('/', uploadFile.single('imgFile'), productsController.store)
-router.get('/detail/:id', productsController.detail)
-router.get('/detail/edit/:id', productsController.edit)
-router.put('/detail/edit/:id', uploadFile.single('imgFile'), productsController.update)
-router.delete('/detail/delete/:id', productsController.destroy)
-
-
 module.exports = router;
